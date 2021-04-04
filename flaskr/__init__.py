6e39a77a82c2099535335bb6c0b6b01cb95aba54
rscotchmer <raescotchmer@gmail.com>
2021-04-04 12:06:55 -0400

    .

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -9,47 +9,44 @@ class GeneratorForm(Form):
     generation = SelectField(label="Generation", choices=["I", "II", "III", "IV", "V", "VI", "VII", "VIII"])
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY="Pokemon"
-    )
+app = Flask(__name__)
+app.config.from_mapping(SECRET_KEY="Pokemon")
 
-    @app.route("/", methods=["GET", "POST"])
-    def index():
-        generatorForm = GeneratorForm()
-        pokemon_data = ""
-        number_to_generate = 1
-        generation = 2
-        egg_move_chance = 0
-        hidden_ability_chance = 0
-        shiny_chance = 0
 
-        if request.method == "GET":
-            return render_template("index.html", generatorForm=generatorForm, pokemon_data=pokemon_data)
-        else:
-            data = request.form
+@app.route("/", methods=["GET", "POST"])
+def index():
+    generatorForm = GeneratorForm()
+    pokemon_data = ""
+    number_to_generate = 1
+    generation = 2
+    egg_move_chance = 0
+    hidden_ability_chance = 0
+    shiny_chance = 0
 
-            if data["number_to_generate"] != "" and data["number_to_generate"] != "0":
-                number_to_generate = int(data["number_to_generate"])
+    if request.method == "GET":
+        return render_template("index.html", generatorForm=generatorForm, pokemon_data=pokemon_data)
+    else:
+        data = request.form
 
-            if data["generation"] != "" and data["generation"] != "0":
-                generation = int(data["generation"])
+        if data["number_to_generate"] != "" and data["number_to_generate"] != "0":
+            number_to_generate = int(data["number_to_generate"])
 
-            if data["egg_move_chance"] != "" and data["egg_move_chance"] != "0":
-                egg_move_chance = int(data["egg_move_chance"])
+        if data["generation"] != "" and data["generation"] != "0":
+            generation = int(data["generation"])
 
-            if data["hidden_ability_chance"] != "" and data["hidden_ability_chance"] != "0":
-                hidden_ability_chance = int(data["hidden_ability_chance"])
+        if data["egg_move_chance"] != "" and data["egg_move_chance"] != "0":
+            egg_move_chance = int(data["egg_move_chance"])
 
-            if data["shiny_chance"] != "" and data["shiny_chance"] != "0":
-                shiny_chance = int(data["shiny_chance"])
+        if data["hidden_ability_chance"] != "" and data["hidden_ability_chance"] != "0":
+            hidden_ability_chance = int(data["hidden_ability_chance"])
 
-            pokemon_data = pokemon_generator.generate_pokemon(number_to_generate, generation, egg_move_chance, hidden_ability_chance, shiny_chance)
-            return render_template("index.html", generatorForm=generatorForm, pokemon_data=pokemon_data)
+        if data["shiny_chance"] != "" and data["shiny_chance"] != "0":
+            shiny_chance = int(data["shiny_chance"])
 
-    return app
+        pokemon_data = pokemon_generator.generate_pokemon(number_to_generate, generation, egg_move_chance,
+                                                          hidden_ability_chance, shiny_chance)
+        return render_template("index.html", generatorForm=generatorForm, pokemon_data=pokemon_data)
 
 
 if __name__ == '__main__':
-    create_app()
+    app.run()
